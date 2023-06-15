7557887ac0296715ba2f95629332882a54c6a5e3
Nickanzo <nicoeanzola@gmail.com>
2023-06-14 22:07:32 -0300

    Criação de Garagem para armazenamento de Carros e Motos

--- Garagem.java
+++ Garagem.java
@@ -1,2 +1,62 @@
+import Veiculo.Carro;
+import Veiculo.Moto;
+
+import java.util.ArrayList;
+
 public class Garagem {
+
+    ArrayList<Carro> listaCarros;
+    ArrayList<Moto> listaMotos;
+
+    public Garagem(){
+        this.listaCarros = new ArrayList<>();
+        this.listaMotos = new ArrayList<>();
+    }
+
+    public Garagem(ArrayList<Carro> listaCarros, ArrayList<Moto> listaMotos) {
+        this.listaCarros = listaCarros;
+        this.listaMotos = listaMotos;
+    }
+
+    public Carro buscaCarro(String placa){
+        for (Carro c : listaCarros){
+            if (c.getPlaca().equals(placa)){
+                return c;
+            }
+        }
+        return null;
+    }
+    public Moto buscaMoto(String placa){
+        for (Moto m : listaMotos){
+            if (m.getPlaca().equals(placa)){
+                return m;
+            }
+        }
+        return null;
+    }
+    public void retiraCarro(String placa){
+        listaCarros.remove(buscaCarro(placa));
+    }
+    public void recebeCarro(Carro c){
+        listaCarros.add(c);
+    }
+
+    public void retiraMoto(String placa){
+        listaMotos.remove(buscaMoto(placa));
+    }
+
+    public void recebeMoto(Moto m){
+        listaMotos.add(m);
+    }
+
+    public void printaCarros() {
+        for (Carro c : listaCarros) {
+            System.out.println(c.toString());
+        }
+    }
+    public void printaMotos(){
+        for (Moto m : listaMotos){
+            System.out.println(m.toString());
+        }
+    }
 }
