3eddd24cec2b66379c5a30345e7d130a8dd502bf
Nickanzo <nicoeanzola@gmail.com>
2023-06-14 22:50:37 -0300

    Criação de Cliente

--- Cliente.java
+++ Cliente.java
@@ -1,2 +1,39 @@
 public class Cliente {
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String email;
+
+    public Cliente(String nome, String endereco, String telefone, String email) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
