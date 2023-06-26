d9d6e0997084193f45333f1f6246d042d2cf3602
Nickanzo <nicoeanzola@gmail.com>
2023-06-26 00:31:33 -0300

    Atualização Venda

--- Venda/Venda.java
+++ Venda/Venda.java
@@ -3,32 +3,26 @@ package Venda;
 import State.VendaState;
 import Veiculo.Veiculo;
 
-import java.util.ArrayList;
 import java.util.Date;
 
 public class Venda {
     private static int numVenda = 0;
     private Date data;
     private Cliente cliente;
-    private ArrayList<Veiculo> veiculos;
+    private Veiculo veiculo;
     private VendaState estado;
-
-    public VendaState getEstado() {
-        return estado;
-    }
-
-    public void setEstado(VendaState estado) {
-        this.estado = estado;
-    }
-
-    public Venda(Cliente cliente, ArrayList<Veiculo> veiculos) {
+    public Venda(Cliente cliente, Veiculo veiculos) {
         ++numVenda;
         this.data = new Date();
         this.cliente = cliente;
-        this.veiculos = veiculos;
-
+        this.veiculo = veiculos;
     }
-
+    public VendaState getEstado() {
+        return estado;
+    }
+    public void setEstado(VendaState estado) {
+        this.estado = estado;
+    }
     public Date getData() {
         return data;
     }
@@ -37,34 +31,22 @@ public class Venda {
         return cliente;
     }
 
-    public ArrayList<Veiculo> getVeiculos() {
-        return veiculos;
-    }
-
     public double calcularValorTotal() {
         double valorTotal = 0.0;
-        for (Veiculo veiculo : veiculos) {
-            valorTotal += veiculo.calcularPrecoTotal();
-        }
+        valorTotal += veiculo.calcularPrecoTotal();
         return valorTotal;
     }
-
-    public NotaFiscal gerarNotaFiscal(int numero) {
-        double valorTotal = calcularValorTotal();
-        return new NotaFiscal(numero, data, cliente, veiculos, valorTotal);
+    public Veiculo getVeiculo() {
+        return veiculo;
     }
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Data: ").append(data).append("\n");
         sb.append("Venda.Cliente: ").append(cliente.getNome()).append("\n");
         sb.append("Carros:\n");
-        for (Veiculo veiculo : veiculos) {
-            sb.append(veiculo.getModelo()).append(" - ").append(veiculo.getPlaca()).append("\n");
-        }
+        sb.append(veiculo.getModelo()).append(" - ").append(veiculo.getPlaca()).append("\n");
         sb.append("Valor Total: ").append(calcularValorTotal()).append("\n");
-
         return sb.toString();
     }
 }
